0ae574ce1c5a897cd7ededb86d59369b31f9047a
ansndsc <ansndsc@gmail.com>
2021-09-21 10:32:33 -0300

    Adiciona atividade media do aluno de Ananda Souto

--- atividade_lar.js
+++ atividade_lar.js
@@ -5,3 +5,16 @@
 //ETAPA 1 = Elaborar um programa que calcule a média do aluno.
 //ETAPA 2 = Informar se ele foi aprovado(utilizar estrutura condicional)
 //ETAPA 3 = GARANTIR que todos os campos serão preenchidos (Utilizar return)
+
+aprovado = "Parabéns, você foi aprovado!";
+reprovado = "Infelizmente, você foi reprovado. Nos vemos no próximo semestre.";
+let calculoMedia = (nota1,nota2,nota3) => (nota1+nota2+nota3)/3;
+let lanZhan = calculoMedia(10,10,10);
+let bingHe = calculoMedia(7,5,8.8)
+let huaCheng = calculoMedia(8,3,4)
+let resultadoLanZhan = lanZhan < 7 ? reprovado : aprovado;
+console.log(resultadoLanZhan);
+let resultadobingHe = bingHe < 7 ? reprovado : aprovado;
+console.log(resultadobingHe)
+let resultadohuaCheng = huaCheng < 7 ? reprovado : aprovado;
+console.log(resultadohuaCheng);
